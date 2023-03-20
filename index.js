8dda6f68499c105a3d5bf0c24fa916f01ce8a6fb
Yohanes Oktanio <111877508+yohanesokta@users.noreply.github.com>
2023-03-20 08:29:19 +0000

    Add Server And reloader

--- index.js
+++ index.js
@@ -146,3 +146,20 @@ async function connectToWhatsApp() {
 connectToWhatsApp().catch((err) => {
   console.log("Eneng Error : Reconneting")
 })
+
+// Stay
+
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+
+keepAlive();
+const monitor = new Monitor({
+    website: '',
+    title: 'NAME',
+    interval: 2
+});
+
+monitor.on('up', (res) => console.log(`${res.website} its on.`));
+monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
+monitor.on('error', (error) => console.log(error));

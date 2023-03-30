ff80f040943c745d4c9aee152ba3798d2326cf9c
yohanesokta <yohanesoktayt72@gmail.com>
2023-03-31 03:30:31 +0700

    Maintenance 31 Maret

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const { default: makeWASocket, DisconnectReason, useSingleFileAuthState } = require('@adiwajshing/baileys')
 const { Boom } = require('@hapi/boom')
 const { state, saveState } = useSingleFileAuthState('./login.json')
+let GlobalChat = 0
+
+//asyc function
+
+async function connectToWhatsApp() {
+  //make qr wa
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+    defaultQueryTimeoutMs: undefined
+  })
+
+  //listen connect updated
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update
+    if (connection === "close") {
+      const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
+      console.log('=> WhatsApp Disconect Reconnect')
+      if (shouldReconnect) {
+        connectToWhatsApp()
+      }
+    } else if (connection === 'open') {
+      console.log('koneksi tersambung')
+    }
+  })
+
+  sock.ev.on("creds.update", saveState)
+
 
 // chat gpt import
 
@@ -25,41 +53,18 @@ async function generateResponse(text) {
   return response.data.choices[0].text;
 }
 
-//asyc function
-
-async function connectToWhatsApp() {
-  //make qr wa
-  const sock = makeWASocket({
-    auth: state,
-    printQRInTerminal: true,
-    defaultQueryTimeoutMs: undefined
-  })
-
-  //listen connect updated
-  sock.ev.on("connection.update", (update) => {
-    const { connection, lastDisconnect } = update
-    if (connection === "close") {
-      const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
-      console.log('Terputusnya sambungan seperti dia : ', lastDisconnect.error, ' Reconnecting', shouldReconnect)
-      if (shouldReconnect) {
-        connectToWhatsApp()
-      }
-    } else if (connection === 'open') {
-      console.log('koneksi tersambung')
-    }
-  })
-
-  sock.ev.on("creds.update", saveState)
 
   //fungsi penangkap pesan
+  Maintenance = "31 Maret 2023"
+  GlobalTextMenu = "Selamat Datang Di Bot Yohanes\n*GUNAKAN FITUR DENGAN BIJAK*\n\n1.Kirim pesan dan akhiri dengan tanda tanya\n  (?) untuk jawaban otomatis\n2. (/menu) untuk membuka menu\n3. (/info) untuk testing server\n\n ON GITHUB : https://github.com/yohanesokta"
+  GlobalFooterGPT = "\n Ketik /menu untuk mengetahui fitur lebih banyak \n *BOT Yohanes Oktanio | GPT4*"
 
   sock.ev.on("messages.upsert", async ({ messages, type }) => {
-    console.log('tipe : ', type)
     if (type === "notify") {
       console.log('-------------------------------------------')
       try {
         const senderNumber = messages[0].key.remoteJid
-
+          GlobalChat += 1
         //dapatkan pesan
         let incomingMessages = messages[0].message.conversation
         if (incomingMessages === "") {
@@ -72,13 +77,16 @@ async function connectToWhatsApp() {
         console.log("nomor : ", senderNumber)
         console.log('IsI : ', incomingMessages)
 
+
+  ID  = messages[0].key.id
+  GlobalTextInfo = "*Sever Rendered*⚠️\n~Maintenance On "+ Maintenance +"~ \nServer On Replit \nID "+ID+"\nPesan Ke : "+GlobalChat+"\n\nWellcome to Yohanes Bot 🇮🇩"
         //if send
-        if (isMessageFromGroup && isMessageMentionBoot && !incomingMessages.includes('/menu')) {
+        if (isMessageFromGroup && isMessageMentionBoot && !incomingMessages.includes('/menu') && incomingMessages.includes('?')) {
           async function main() {
             const result = await generateResponse(incomingMessages)
             await sock.sendMessage(
               senderNumber,
-              { text: result + '\n\n _YohanesOktanio | bot_' },
+              { text: result + GlobalFooterGPT },
               { quoted: messages[0] },
               2000
             )
@@ -88,7 +96,7 @@ async function connectToWhatsApp() {
         if (isMessageMentionBoot && incomingMessages.includes('/menu')) {
           await sock.sendMessage(
             senderNumber,
-            { text: "*Yohanes Oktanio BOT*\n ~ mention saya dan tanya apapun untuk activasi chat gpt \n\n _sekian terimakasih_ " },
+            {text: GlobalTextMenu},
             { quoted: messages[0] },
             2000
           )
@@ -96,27 +104,27 @@ async function connectToWhatsApp() {
         if (!isMessageFromGroup && incomingMessages.includes('/menu')) {
           await sock.sendMessage(
             senderNumber,
-            { text: "*Yohanes Oktanio BOT (tested)*\n akhiri apapun dengan tanda tanya \n\n _sekian terimakasih_ " },
+            {text: GlobalTextMenu},
             { quoted: messages[0] },
             2000
           )
         }
         if (!isMessageFromGroup && incomingMessages.includes('?')) {
-         async function msg(){ 
-          await sock.sendMessage(
-            senderNumber,
-            { text: "*Bot Sedang Berfikir..*" },
-            { quoted: messages[1] },
-            2000
-          )
-         }
+          async function msg() {
+            await sock.sendMessage(
+              senderNumber,
+              { text: "*Mencari Jawaban Yang Tepat 😇*" },
+              { quoted: messages[1] },
+              2000
+            )
+          }
 
           async function main() {
             const result = await generateResponse(incomingMessages)
             console.log(result)
             await sock.sendMessage(
               senderNumber,
-              { text: result + '\n\n _YohanesOktanio | bot_' },
+              { text: result + GlobalFooterGPT },
               { quoted: messages[0] },
               2000
             )
@@ -127,17 +135,18 @@ async function connectToWhatsApp() {
         if (!isMessageFromGroup && incomingMessages.includes('/info')) {
           await sock.sendMessage(
             senderNumber,
-            { text: "*Yohanes Oktanio 18 maret 2023 \n\n _sekian terimakasih_ " },
+            {text: GlobalTextInfo},
             { quoted: messages[0] },
             2000
           )
         }
-
+        console.log('-------------------------------------------')
 
 
       }
       catch (error) {
         console.log("Error : Pesan Bukan Tulisan")
+        console.log('-------------------------------------------')
       }
     }
   })
@@ -154,12 +163,12 @@ const Monitor = require('ping-monitor');
 
 keepAlive();
 const monitor = new Monitor({
-    website: '',
-    title: 'NAME',
-    interval: 2
+  website: '',
+  title: 'NAME',
+  interval: 2
 });
 
-monitor.on('up', (res) => console.log(`${res.website} its on.`));
-monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
-monitor.on('error', (error) => console.log(error));
+monitor.on('up', (res) => console.log('its on.'));
+monitor.on('down', (res) => console.log(`it has died `));
+monitor.on('stop', (website) => console.log(`has stopped`));
+monitor.on('error', (error) => console.log("Monitor error"));
